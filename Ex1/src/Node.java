c72b91fcef1d1d162e607c27e0ac9a6d143c8bde
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-06 15:36:47 +0200

    Added 2nd constructor with "deep copy"

--- Ex1/src/Node.java
+++ Ex1/src/Node.java
@@ -2,9 +2,15 @@ public class Node {
     private Board board;
     private Node[] next;
 
-    public Node(Board b)
+    public Node(Board b) // constructor to start node
     {
         this.board = b;
+        this.next = new Node[4];
+    }
+
+    public Node(String b, String w)
+    {
+        this.board = new Board(b, w);
         this.next = new Node[4];
     }
 }
