c5dde537784034ef0ed06a9cff7aa60699d37504
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-12 15:38:39 +0200

    Basic solve function

--- Ex1/src/Solver.java
+++ Ex1/src/Solver.java
@@ -1,9 +1,13 @@
 public class Solver {
 
+    private Node start;
     private String algorithm;
     private boolean withTime;
     private boolean printOpenList;
+    private int cost = 0;
+    private int numOfNodes = 0;
     private Board board;
+
 
     public Solver(Board b, String alg, String wt, String pol)
     {
@@ -11,11 +15,16 @@ public class Solver {
         this.algorithm = alg;
         this.withTime = wt.contains("no") ? false : true;
         this.printOpenList = pol.contains("no") ? false : true;
+        this.start = new Node(this.board);
     }
 
-    public void solve(Board b, String alg)
+    public void solve(String alg)
     {
-        // TODO
+        Node goal = new Node(new Board(Algorithms.goalString(this.board.getSize()), ""));
+        String solution;
+        if (alg.equals("DFID")) {
+            solution = Algorithms.dfid(this.start, goal);
+        }
     }
 
 }
