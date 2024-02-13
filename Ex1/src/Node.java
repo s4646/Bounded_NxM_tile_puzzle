0794a42a74493a93c276fd7520de3b6d29ccb636
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-13 04:52:07 +0200

    Modified Node class

--- Ex1/src/Node.java
+++ Ex1/src/Node.java
@@ -1,24 +1,42 @@
+import java.util.LinkedList;
+
 public class Node {
     private Board board;
-    private Node[] next;
+    private Node prev;
+    private LinkedList<Node> next;
+    private String operation;
 
     public Node(Board b) // constructor to start node
     {
         this.board = b;
+        this.prev = null;
+        this.next = new LinkedList<Node>();
     }
 
     public Node(String b, String w)
     {
         this.board = new Board(b, w);
+        this.prev = null;
+        this.next = new LinkedList<Node>();
     }
 
     public Board getBoard()
     {
         return this.board;
     }
-    
-    public void createNextArray(int size)
+
+    public void addNext(Node n)
     {
-        this.next = new Node[size];
+        this.next.add(n);
+    }
+
+    public void setPrev(Node n)
+    {
+        this.prev = n;
+    }
+
+    public Node getPrev()
+    {
+        return this.prev;
     }
 }
