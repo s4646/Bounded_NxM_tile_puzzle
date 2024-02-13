a26cff8e65f03f3503d08b9c36396d0a62832633
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-13 05:00:30 +0200

    Test case example

--- Ex1/src/Main.java
+++ Ex1/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+// import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,20 +6,27 @@ public class Main {
         InputReader myReader = new InputReader();
         myReader.getInput("input.txt");
         System.out.println(myReader.toString());
-        // System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        
         Board myBoard = new Board(myReader.getBoard(), myReader.getWhiteTiles());
-        System.out.println(myBoard.toString());
+        Solver mySolver = new Solver(myBoard, myReader.getAlgorithm(), myReader.getWithTime(), myReader.getPrintOpenList());
+        mySolver.solve();
+        System.out.println(mySolver.getSolution());
 
-        Node n = new Node(myBoard);
-        LinkedList<Character> l = Algorithms.getValidOperators(n);
-        System.out.println(n.getBoard().getTiles());
-        for (Character character : l) {
-            System.out.println(character);
-            Node test = Algorithms.operate(n, character);
-            // System.out.println(test.getBoard().getBoard());
-            System.out.println(test.getBoard().toString());
-            // System.out.println(test.getBoard().whiteTilesToString());
-        }
 
+        
+        // System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        // Board myBoard = new Board(myReader.getBoard(), myReader.getWhiteTiles());
+        // System.out.println(myBoard.toString());
+
+        // Node n = new Node(myBoard);
+        // LinkedList<Character> l = Algorithms.getValidOperators(n);
+        // System.out.println(n.getBoard().getTiles());
+        // for (Character character : l) {
+        //     System.out.println(character);
+        //     Node test = Algorithms.operate(n, character);
+        //     // System.out.println(test.getBoard().getBoard());
+        //     System.out.println(test.getBoard().toString());
+        //     // System.out.println(test.getBoard().whiteTilesToString());
+        // }
     }
 }
