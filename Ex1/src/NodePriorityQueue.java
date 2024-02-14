a3b7f8b22389d1088bc4b6a66e882c584f8902b6
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-15 01:42:16 +0200

    Implementation of node priority queue

---
+++ Ex1/src/NodePriorityQueue.java
@@ -0,0 +1,35 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class NodePriorityQueue extends PriorityQueue<Node> implements Comparator<Node>{
+        
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Node)) {
+            return false;
+        }
+        Node n = (Node) obj;
+        return this.toString().equals(n.toString());
+    }
+
+    @Override
+    public int compare(Node n1, Node n2)
+    {
+       return Integer.compare(n1.getCost(), n2.getCost());
+    }
+
+    // public Node get(Node n)
+    // {
+    //     for (Node item : this) {
+    //         if (n.toString().equals(item.toString()))
+    //             return item;
+    //     }
+    //     return null;
+    // }
+
+    // public int ManhattenDistance(int value, int rowIndex, int colIndex, int[] )
+    // {
+
+    // }
+}
