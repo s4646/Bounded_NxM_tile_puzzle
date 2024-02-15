3d07895536edc708c9bfee9106cfef6b24212b5d
Sahar Tuvyahu <sahar45456@gmail.com>
2024-02-15 20:59:15 +0200

    Node is no longer comparable
    For now

--- Ex1/src/Node.java
+++ Ex1/src/Node.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 
-public class Node implements Comparable<Node>{
+public class Node {
     private Board board;
     private Node prev;
     private LinkedList<Node> next;
@@ -65,33 +65,9 @@ public class Node implements Comparable<Node>{
         return this.cost;
     }
 
-    public int ManhattenDistance(Node n) // Heuristic Function
-    {
-        int dist = 0;
-        int [] boardSize = this.getBoard().getSize();
-        Tile[][] tiles = n.getBoard().getTiles();
-        
-        for (int i = 0; i < boardSize[0]; i++) {                 // rows -> Y
-            for (int j = 0; j < boardSize[1]; j++) {             // cols -> X
-                 if (tiles[i][j].getType() == 2) // Empty Tile
-                    continue;
-                int value = Integer.parseInt(tiles[i][j].getContent());
-                dist += Math.abs(i - (value-1) / boardSize[0]) + Math.abs(j - (value-1) % boardSize[1]);
-            
-            }
-        }
-        return dist;
-    }
-
     @Override
     public String toString()
     {
         return this.board.toString();
-    }
-
-    @Override
-    public int compareTo(Node ot)
-    {
-        return Integer.compare(this.cost + ManhattenDistance(this), ot.getCost() + ManhattenDistance(ot));
     }
 }
